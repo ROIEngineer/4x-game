f38acce3aae2b777ac0c69ea9ac8b43e1310a151
ROIEngineer <191797684+ROIEngineer@users.noreply.github.com>
2026-03-27 23:33:05 -0400

    Remove irrelevant properties from Nation class

--- src/entities/Nation.js
+++ src/entities/Nation.js
@@ -3,20 +3,8 @@ class Nation {
         this.id = id;
         this.name = name;
         this.gold = 0;
+        this.population = 0;
+        this.stability = 0;
         this.provinceIds = [];
-        this.stability = 100;
-    }
-
-    addProvince(province) {
-        this.provinces.push(province);
-    }
-
-    removeProvince(provinceName) {
-        this.provinces = this.provinces.filter(p => p.name !== provinceName);
-    }
-
-    receiveCommand(command) {
-        // For now, just pass it to systems
-        CommandSystem.handleCommand(this, command);
     }
 }
